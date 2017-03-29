cc302e516113a1af300045e19b313bcaccffe272
gfwlwd <gfwlwd@hotmail.com>
2017-03-29 02:43:30 +0200

    Added view

--- ios/src/com/grardweijer/mariobros/IOSLauncher.java
+++ ios/src/com/grardweijer/mariobros/IOSLauncher.java
@@ -1,11 +1,10 @@
 package com.grardweijer.mariobros;
 
-import org.robovm.apple.foundation.NSAutoreleasePool;
-import org.robovm.apple.uikit.UIApplication;
-
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.grardweijer.mariobros.MarioBros;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
